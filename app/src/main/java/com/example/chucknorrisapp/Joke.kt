5632694fa1950e516f6a0394b46bc5a14896fe06
Marius Morel <marius.morel@edu.esiee.fr>
2022-05-15 10:51:42 +0200

    Part 2.2 Retrofit & Rx Java

--- app/src/main/java/com/example/chucknorrisapp/Joke.kt
+++ app/src/main/java/com/example/chucknorrisapp/Joke.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
+
 @Serializable
 class Joke(val categories:List<String>,@SerialName("created_at") val createdAt: String, @SerialName("icon_url") val iconUrl:String, val id :String, @SerialName("updated_at") val updatedAt:String, val url : String, val value : String)
 
